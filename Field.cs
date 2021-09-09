6a516755905800b4c3bcb6650f69c4c6d10e9a82
hnlvc <saschad.heinl96@gmail.com>
2021-09-09 19:15:59 +0200

    Projektdateien hinzufügen.

---
+++ Field.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace ConwaysGameOfLife
+{
+    class Field
+    {
+        public Cell[,] CurrentGenField { get => currentGenField; }
+        private Cell[,] currentGenField;
+        private Cell[,] nextGenField;
+        private int length;
+        private int height;
+
+        public Field(Cell[,] currentGenField)
+        {
+            var length = currentGenField.GetLength(0);
+            var height = currentGenField.GetLength(1);
+            
+            this.currentGenField = currentGenField;
+            nextGenField = new Cell[length, height];
+            
+        }
+
+
+        public void CalculateNextGen()
+        {
+            for (var length = 0; length < this.length; ++length)
+            {
+                for (var height = 0; height < this.height; ++height)
+                {
+                    var alive = CountLivingNeighbors(length, height);
+                    if (CurrentGenField[length, height].IsAlive)
+                    {
+                        if ((alive == 2 || alive == 3))
+                        {
+                            nextGenField[length, height].IsAlive = true;
+                        }
+                    }
+
+                    if (CurrentGenField[length, height].IsAlive == false)
+                    {
+                        if (alive == 3)
+                        {
+                            nextGenField[length, height].IsAlive = true;
+                        }
+                    }
+                }
+            }
+            this.currentGenField = nextGenField;
+        }
+
+        public int CountLivingNeighbors(int posLength, int posHeight)
+        {
+            var counter = 0;
+
+            if (IsInField(posLength - 1, posHeight - 1))
+            {
+                if (currentGenField[posLength - 1, posHeight - 1].IsAlive) counter++;
+            }
+            if (IsInField(posLength, posHeight - 1))
+            {
+                if (currentGenField[posLength, posHeight - 1].IsAlive) counter++;
+            }
+            if (IsInField(posLength + 1, posHeight - 1))
+            {
+                if (currentGenField[posLength + 1, posHeight - 1].IsAlive) counter++;
+            }
+            if (IsInField(posLength + 1, posHeight))
+            {
+                if (currentGenField[posLength + 1, posHeight].IsAlive) counter++;
+            }
+            if (IsInField(posLength + 1, posHeight + 1))
+            {
+                if (currentGenField[posLength + 1, posHeight + 1].IsAlive) counter++;
+            }
+            if (IsInField(posLength, posHeight + 1))
+            {
+                if (currentGenField[posLength, posHeight + 1].IsAlive) counter++;
+            }
+            if (IsInField(posLength - 1, posHeight + 1))
+            {
+                if (currentGenField[posLength - 1, posHeight + 1].IsAlive) counter++;
+            }
+            if (IsInField(posLength - 1, posHeight))
+            {
+                if (currentGenField[posLength - 1, posHeight].IsAlive) counter++;
+            }
+
+            return counter;
+        }
+
+        public bool IsInField(int length, int height)
+        {
+            if (!(length >= 0 && length < this.length)) return false;
+            if (!(height >= 0 && height < this.height)) return false;
+
+            return true;
+        }
+
+        public void Print()
+        {
+            string line;
+
+            for (var height = 0; height < this.height; ++height)
+            {
+                line = "";
+                for (var length = 0; length < this.length; ++length)
+                {
+                    line = string.Concat(line, currentGenField[length, height].IsAlive ? '#' : '-');
+                }
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace ConwaysGameOfLife
+{
+    class Program
+    {
+        static int fieldSizeX = 60;
+        static int fieldSizeY = 60;
+        
+        static void Main(string[] args)
+        {
+            Cell[,] field = new Cell[fieldSizeX, fieldSizeY]; // cell[,] field = new [x, y];+6
+
+            //Initialize first gen with a rider in upper left corner
+
+            for (var i = 0; i > fieldSizeX; ++i)
+            {
+                for (var j = 0; j > fieldSizeY; ++j)
+                {
+                    field[i, j] = new Cell(false);
+                }
+            }
+            
+            
+            field[1, 0] = new Cell(true);
+            field[2, 1] = new Cell(true);
+            field[0, 2] = new Cell(true);
+            field[1, 2] = new Cell(true);
+            field[2, 2] = new Cell(true);
+
+            var field1 = new Field(field);
+
+            while (true)
+            {
+                Console.Clear();
+                field1.CalculateNextGen();
+                field1.Print();
+                Thread.Sleep(1000000);
+            }
+
+        }
+    }
+}
